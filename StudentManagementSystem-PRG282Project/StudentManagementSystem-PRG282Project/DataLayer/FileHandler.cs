65d8b389d10c272c3057bfa061c08f90b74ce450
Goldfish2574 <142659285+Goldfish2574@users.noreply.github.com>
2024-11-02 12:56:02 +0200

    Updated filehandler class to read students.txt

--- StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/FileHandler.cs
+++ StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/FileHandler.cs
@@ -5,18 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Configuration;
 
 namespace StudentManagementSystem_PRG282Project.DataLayer
 {
     internal class FileHandler
     {
         string path = @"students.txt";
+        public List<Student> students = new List<Student>();
+        
+
 
         public void TestFile()
         {
             if (File.Exists(path))
             {
                 MessageBox.Show("I exist");
+            }
+        }
+
+        public void Reader()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] lines = line.Split(',');
+                        Student StudentRead = new Student(int.Parse(lines[0]), lines[1], lines[2], DateTime.Parse(lines[3]), lines[4], lines[5], lines[6], lines[7]);
+                        students.Add(StudentRead);
+                        
+                    }
+
+                }
+            }
+            //Test Display
+            foreach (var item in students)
+            {
+                MessageBox.Show(item.ToString());
             }
         }
     }
--- StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/Student.cs
+++ StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/Student.cs
@@ -14,9 +14,10 @@ namespace StudentManagementSystem_PRG282Project.DataLayer
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Course { get; set; }
+        public string Address { get; set; }
         public string CellNum { get; set; }
 
-        public Student(int studentID, string firstname, string lastname, DateTime dateofbirth, string email, string course, string cellnum) 
+        public Student(int studentID, string firstname, string lastname, DateTime dateofbirth, string email, string course, string address, string cellnum) 
         {
             StudentID = studentID;
             FirstName = firstname;
@@ -24,7 +25,14 @@ namespace StudentManagementSystem_PRG282Project.DataLayer
             DateOfBirth = dateofbirth;
             Email = email;
             Course = course;
+            Address = address;
             CellNum = cellnum;
+        }
+
+        public override string ToString()
+        {
+            return $"Student ID: {StudentID}\nFirst Name: {FirstName}Last Name: {LastName}\nDate of Birth: {DateOfBirth}\nDate of Birth: {DateOfBirth}\nEmail: {Email}\nCourse: {Course}\nAddress: {Address}\nCell Number: {CellNum}";
+
         }
     }
 }
