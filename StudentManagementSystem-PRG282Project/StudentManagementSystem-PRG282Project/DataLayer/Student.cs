e97dd6f0a97c2ec270a07be5e16049a293c3a04c
Goldfish2574 <142659285+Goldfish2574@users.noreply.github.com>
2024-11-02 12:34:56 +0200

    Removed headings in student.txt and added student.cs data

--- StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/Student.cs
+++ StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/DataLayer/Student.cs
@@ -8,5 +8,23 @@ namespace StudentManagementSystem_PRG282Project.DataLayer
 {
     internal class Student
     {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public string Course { get; set; }
+        public string CellNum { get; set; }
+
+        public Student(int studentID, string firstname, string lastname, DateTime dateofbirth, string email, string course, string cellnum) 
+        {
+            StudentID = studentID;
+            FirstName = firstname;
+            LastName = lastname;
+            DateOfBirth = dateofbirth;
+            Email = email;
+            Course = course;
+            CellNum = cellnum;
+        }
     }
 }
