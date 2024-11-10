2814f79a35eb75cf35189bf4c83b3b651dc888a0
EJ-Engelbrecht <142659285+EJ-Engelbrecht@users.noreply.github.com>
2024-11-10 16:20:05 +0200

    Added form update to summaryform

--- StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/PresentationLayer/SummaryForm.Designer.cs
+++ StudentManagementSystem-PRG282Project/StudentManagementSystem-PRG282Project/PresentationLayer/SummaryForm.Designer.cs
@@ -29,35 +29,26 @@
         private void InitializeComponent()
         {
             this.totalLbl = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnReturn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // totalLbl
             // 
             this.totalLbl.AutoSize = true;
             this.totalLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.totalLbl.Location = new System.Drawing.Point(46, 174);
+            this.totalLbl.Location = new System.Drawing.Point(12, 209);
             this.totalLbl.Name = "totalLbl";
             this.totalLbl.Size = new System.Drawing.Size(94, 29);
             this.totalLbl.TabIndex = 0;
             this.totalLbl.Text = "lblTotal";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(246, 28);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(225, 36);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Summary report";
-            // 
             // btnReturn
             // 
             this.btnReturn.BackColor = System.Drawing.Color.SteelBlue;
             this.btnReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReturn.Location = new System.Drawing.Point(44, 389);
+            this.btnReturn.Location = new System.Drawing.Point(12, 291);
             this.btnReturn.Name = "btnReturn";
             this.btnReturn.Size = new System.Drawing.Size(86, 44);
             this.btnReturn.TabIndex = 3;
@@ -65,19 +56,30 @@
             this.btnReturn.UseVisualStyleBackColor = false;
             this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StudentManagementSystem_PRG282Project.Properties.Resources.SummaryReport;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(495, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // SummaryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(817, 556);
+            this.ClientSize = new System.Drawing.Size(709, 387);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnReturn);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.totalLbl);
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Name = "SummaryForm";
-            this.Text = "SummaryForm";
+            this.Text = "\\";
             this.Load += new System.EventHandler(this.SummaryForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,7 +88,7 @@
         #endregion
 
         private System.Windows.Forms.Label totalLbl;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
